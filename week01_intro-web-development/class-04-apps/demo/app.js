4be5c2cfd1d3f45ed16fad2f89c2c564a766bb75
martypdx <marty@martypdx.com>
2018-07-26 11:27:48 -0700

    get two random students for one pair

--- week01_intro-web-development/class-04-apps/demo/app.js
+++ week01_intro-web-development/class-04-apps/demo/app.js
@@ -9,12 +9,27 @@ function makePairs() {
     // we need to be able to run the process again
     var copy = students.slice();
 
-    console.log('random number', getRandomInt(13));
+    console.log('random number', getRandomIndex(13));
 
     // track number of times this function has been called
     tries += 1;
 
-    // randomization (Sarah sez checkout Math.random on mdn)
+    function getRandomStudent() {
+        var indexOne = getRandomIndex(copy.length);
+        // get student before we remove them
+        var studentOne = copy[indexOne];
+        // remove from the array
+        copy.splice(indexOne, 1);
+
+        return studentOne;
+    }
+
+    // make one pair
+    var studentOne = getRandomStudent();
+    var studentTwo = getRandomStudent();
+
+    
+
     // how to not reassign people or match people to self?
         // 1. remove from array
 
@@ -24,14 +39,15 @@ function makePairs() {
     // display results
         // pairs
             // manage reset (old pairs gone)
-
+    // display the one pair
+    document.getElementById('pair-0').innerText = studentOne + ' - ' + studentTwo; 
     // update tries
     document.getElementById('tries').innerText = tries;
 
 }
 
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-function getRandomInt(max) {
+function getRandomIndex(max) {
     // The maximum is exclusive and the minimum (0) is inclusive
     return Math.floor(Math.random() * max); 
 }
