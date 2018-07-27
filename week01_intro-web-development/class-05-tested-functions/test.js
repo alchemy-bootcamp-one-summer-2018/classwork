0064f8254563b5ff0059551a61cdd9c937838b80
martypdx <marty@martypdx.com>
2018-07-26 22:20:07 -0700

    test and runner library

--- week01_intro-web-development/class-05-tested-functions/test.js
+++ week01_intro-web-development/class-05-tested-functions/test.js
@@ -1,5 +1,6 @@
+'use strict';
+/* globals assert */
 /* exported assert, test */
-
 
 var tests = [];
 
@@ -10,26 +11,73 @@ function test(name, testFn) {
     });
 }
 
-function run() {
+test.run = function() {
+    var testResults = runTests();
+    displayResults(testResults);
+};
+
+var PASS = {
+    text: 'PASS',
+    color: 'green'
+};
+
+var FAIL = {
+    text: 'FAIL',
+    color: 'red'
+};
+
+
+function runTests() {
     const results = [];
 
     for(var i = 0; i < tests.length; i++) {
         var test = tests[i];
 
         try {
-            test();
-            results.push({
-                test: test.name,
-                result: 'PASSED'
-            });
+            test.testFn();
+            test.status = PASS;
         }
         catch(err) {
-            results.push({
-                test: test.name,
-                result: 'FAILED'
-            });
+            test.status = FAIL;
+            test.error = err.message;
         }
+
+        results.push(test);
     }
 
     return results;
+}
+
+function displayResults(tests) {
+    
+    for(var i = 0; i < tests.length; i++) {
+        var test = tests[i];
+
+        console.log(
+            test.name + ' %c' + test.status.text,
+            'color: ' + test.status.color + ';'
+        );
+
+        if(test.error) {
+            console.log(
+                '\t%c' + test.error || '', 
+                'color: red;'
+            );
+        }
+    }
+}
+
+
+// change to true to run pass and fail test tests
+if(false) { // eslint-disable-line
+
+    test('1 + 1 = 2 passes', () => {
+        assert.equal(1 + 1, 2);
+    });
+
+    test('1 + 1 = 2 fails', () => {
+        assert.equal(1 + 1, 3);
+    });
+
+    test.run();
 }
