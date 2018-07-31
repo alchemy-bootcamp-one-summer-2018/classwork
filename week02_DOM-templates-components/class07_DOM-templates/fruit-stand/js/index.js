ec83ac8f7b90ddbd6e6efc53033b06cdc4caeaa2
martypdx <marty@martypdx.com>
2018-07-31 11:58:52 -0700

    final changes

--- week02_DOM-templates-components/class07_DOM-templates/fruit-stand/js/index.js
+++ week02_DOM-templates-components/class07_DOM-templates/fruit-stand/js/index.js
@@ -2,41 +2,37 @@
 
 (function(module){
     var fruits = module.fruits;
+    var toDOM = module.toDOM;
+    var html = module.html;
 
     // reference the ul
     var ul = document.getElementById('fruits');
 
-    // reference the template
-    var template = document.getElementById('fruit-template').content;
+    // create a function we can call with data,
+    // that returns DOM we can append into the 
+    // document
+    var render = function(fruit) {
+        return toDOM(html`        
+            <li>
+                <h2>${fruit.name}</h2>
+                <img src="${fruit.image}" alt="${fruit.name}">
+                <span class="color-name" 
+                    style="background: ${fruit.color}">
+                    ${fruit.color}
+                </span>
+            </li>
+        `);
+    };
 
     // loop each fruit
     var fruit;
     for(var i = 0; i < fruits.length; i++) {
         fruit = fruits[i];
+        // make a fruit template instance
+        var dom = render(fruit);
 
-        // make a "li" that has this fruit's data
-        var dom = template.cloneNode(true);
-
-        // set header
-        var h2 = dom.querySelector('h2');
-        h2.textContent = fruit.name;
-
-        // set img src
-        var img = dom.querySelector('img');
-        img.src = fruit.image;
-        img.alt = fruit.name;
-
-        // set span text and style color
-        var span = dom.querySelector('span');
-        span.textContent = fruit.color;
-        span.style.background = fruit.color;
-        
-        console.log('doc frag', dom.childNodes.length);
         // append it to the ul
         ul.appendChild(dom);
-
-        console.log('doc frag', dom.childNodes.length);
     }
-
 
 })(window.module = window.module || {});
